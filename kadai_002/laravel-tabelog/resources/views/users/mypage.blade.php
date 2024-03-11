ba63a485bf28abfc85543a257a742d7ca4114f1c
Steven Friend <stevenmarkfriend@gmail.com>
2024-03-12 08:05:56 +0900

    マイページの追加

---
+++ kadai_002/laravel-tabelog/resources/views/users/mypage.blade.php
@@ -0,0 +1,106 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container d-flex justify-content-center mt-3">
+    <div class="w-50">
+        <h1>マイページ</h1>
+
+        <hr>
+
+        <div class="container">
+            <div class="d-flex justify-content-between">
+                <div class="row">
+                    <div class="col-2 d-flex align-items-center">
+                        <i class="fas fa-user fa-3x"></i>
+                    </div>
+                    <div class="col-9 d-flex align-items-center ms-2 mt-3">
+                        <div class="d-flex flex-column">
+                            <label>会員情報の編集</label>
+                            <p>アカウント情報の編集</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex align-items-center">
+                    <a href="{{route('mypage.edit')}}">
+                        <i class="fas fa-chevron-right fa-2x"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <hr>
+
+        <div class="container">
+            <div class="d-flex justify-content-between">
+                <div class="row">
+                    <div class="col-2 d-flex align-items-center">
+                        <i class="fas fa-utensils fa-3x"></i>
+                    </div>
+                    <div class="col-9 d-flex align-items-center ms-2 mt-3">
+                        <div class="d-flex flex-column">
+                            <label>予約一覧</label>
+                            <p>予約を確認できます</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex align-items-center">
+                    <a href="{{route('mypage')}}">
+                        <i class="fas fa-chevron-right fa-2x"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <hr>
+
+        <div class="container">
+            <div class="d-flex justify-content-between">
+                <div class="row">
+                    <div class="col-2 d-flex align-items-center">
+                        <i class="fas fa-heart fa-3x"></i>
+                    </div>
+                    <div class="col-9 d-flex align-items-center ms-2 mt-3">
+                        <div class="d-flex flex-column">
+                            <label>気に入ったお店</label>
+                            <p>気に入ったお店を見ます</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex align-items-center">
+                    <a href="{{route('mypage')}}">
+                        <i class="fas fa-chevron-right fa-2x"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <hr>
+        <div class="container">
+            <div class="d-flex justify-content-between">
+                <div class="row">
+                    <div class="col-2 d-flex align-items-center">
+                        <i class="fas fa-sign-out-alt fa-3x"></i>
+                    </div>
+                    <div class="col-9 d-flex align-items-center ms-2 mt-3">
+                        <div class="d-flex flex-column">
+                            <label>ログアウト</label>
+                            <p>ログアウトします</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex align-items-center">
+                    <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        <i class="fas fa-chevron-right fa-2x"></i>
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        <hr>
+    </div>
+</div>
+@endsection
